6dd44f2f3f6d19e718d31120d59781b361bb578c
FabioBjr <fabiobritojunior1101@gmail.com>
2024-06-05 00:39:01 -0300

    inital commit

---
+++ src/Conta.java
@@ -0,0 +1,51 @@
+public abstract class Conta implements IConta{
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void transferir(Conta contaDestino, double valor) {
+         this.sacar(valor);
+         contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.printf("Titular: %s", this.cliente.getNome());
+        System.out.printf("Agencia: %d%n", this.agencia);
+        System.out.printf("Número: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
+    }
+}
